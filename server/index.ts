e09cfbd3d3cc9df9440fea1d5ef45d8a993faa3e
taeyoung-no <notaeyoung03@gmail.com>
2026-06-10 15:23:29 +0900

    MongoDB 연결

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,25 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+mongoose
+  .connect(process.env.MONGODB_URI!)
+  .then(() => console.log('[MongoDB] 연결 성공'))
+  .catch((err) => console.log(`[MongoDB] ${err}`))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const PORT = 8080
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
 })
