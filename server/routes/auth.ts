adef7d897a4448318a199dcff32b260398ab3d9d
taeyoung-no <notaeyoung03@gmail.com>
2026-06-11 13:42:21 +0900

    username 중복인 경우 에러 수정

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -7,10 +7,7 @@ const router = express.Router()
 router.post('/register', async (req, res) => {
   const { username, password } = req.body
 
-  const existingUser = await User.findOne({
-    username: username,
-    password: password,
-  })
+  const existingUser = await User.findOne({ username: username })
   if (existingUser) return res.status(400).json({ message: 'username 중복임' })
 
   const hashedPassword = await bcrypt.hash(password, 10)
