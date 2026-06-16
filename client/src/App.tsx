a98c85304338609c6432d64b9c5d2e84f5579068
taeyoung-no <notaeyoung03@gmail.com>
2026-06-16 12:27:36 +0900

    클라이언트에서 방 목록 조회 기능 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface Room {
+  _id: string
+  name: string
+  createdAt: string
+}
+
 function App() {
+  const [rooms, setRooms] = useState<Room[]>([])
+
+  const fetchRooms = async () => {
+    try {
+      const res = await fetch('http://localhost:8080/room')
+      const data = await res.json()
+      setRooms(data.rooms || [])
+    } catch (err) {
+      console.error(`방 목록 불러오기 실패: ${err}`)
+    }
+  }
+
+  useEffect(() => {
+    fetchRooms()
+  }, [])
+
   return (
-    <div className="px-10 py-5">
-      <h1 className="text-3xl font-bold">Hello World!</h1>
+    <div className="max-w-2xl mx-auto pt-5">
+      <header className="mb-5">
+        <h1 className="text-3xl font-bold text-center">Chat</h1>
+      </header>
+
+      <main className="space-y-2">
+        {rooms.map((room) => (
+          <div key={room._id} className="p-4 border border-gray-300">
+            <h4>{room.name}</h4>
+          </div>
+        ))}
+      </main>
     </div>
   )
 }
