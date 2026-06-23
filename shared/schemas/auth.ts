d1bfcc56e588f9fb7a259ac80c9bd154b5315d0a
taeyoung-no <notaeyoung03@gmail.com>
2026-06-23 15:04:33 +0900

    클라이언트에서 zod user 사용하도록 수정

--- shared/schemas/auth.ts
+++ shared/schemas/auth.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod'
 
+export const userSchema = z.object({
+  _id: z.string(),
+  username: z.string(),
+})
+
 export const registerSchema = z.object({
   username: z.string().trim().min(1, '이름 입력하세요'),
   password: z.string().trim().min(1, '비밀번호 입력하세요'),
@@ -10,5 +15,6 @@ export const loginSchema = z.object({
   password: z.string().trim().min(1, '비밀번호 입력하세요'),
 })
 
+export type User = z.infer<typeof userSchema>
 export type RegisterFormData = z.infer<typeof registerSchema>
 export type LoginFormData = z.infer<typeof loginSchema>
