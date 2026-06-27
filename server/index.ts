c2c4a9ec561fb47aac2bff56f878b65049168208
taeyoung-no <notaeyoung03@gmail.com>
2026-06-27 21:41:15 +0900

    sticky session, redis 도입

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,32 @@
 import mongoose from 'mongoose'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import { createClient } from 'redis'
+import { createAdapter } from '@socket.io/redis-adapter'
 import app from './app.js'
 import { setupSockets } from './socket.js'
 
-mongoose
-  .connect(process.env.MONGODB_URI!)
-  .then(() => console.log('[MongoDB] 연결 성공'))
-  .catch((err) => console.log(`[MongoDB] ${err}`))
+try {
+  await mongoose.connect(process.env.MONGODB_URI!)
+  console.log('[MongoDB] 연결 성공')
+} catch (err) {
+  console.log(`[MongoDB] ${err}`)
+  process.exit(1)
+}
+
+const pubClient = createClient({ url: process.env.REDIS_URL! })
+const subClient = pubClient.duplicate()
+
+pubClient.on('error', (err) => console.log(`[Redis pub] ${err}`))
+subClient.on('error', (err) => console.log(`[Redis sub] ${err}`))
+
+try {
+  await Promise.all([pubClient.connect(), subClient.connect()])
+  console.log('[Redis] 연결 성공')
+} catch (err) {
+  console.log(`[Redis] ${err}`)
+  process.exit(1)
+}
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
@@ -16,6 +35,8 @@ const io = new Server(httpServer, {
     credentials: true,
   },
 })
+
+io.adapter(createAdapter(pubClient, subClient))
 
 app.set('io', io)
 setupSockets(io)
