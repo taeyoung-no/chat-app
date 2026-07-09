2ceac5c4a6e33e30b118e72f422880b8369a9788
taeyoung-no <notaeyoung03@gmail.com>
2026-07-09 15:29:30 +0900

    errorHandler가 AppError만 메시지를 그대로 반환하도록 수정

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
+import AppError from '../utils/AppError.js'
 
-const errorHandler = (err: any, req: Request, res: Response, _: NextFunction) => {
-  res.status(err.status || 500).json({ success: false, message: err.message || '서버 문제인 듯' })
+const errorHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) return res.status(err.status).json({ success: false, message: err.message })
+  console.error('[Error]', err)
+  res.status(500).json({ success: false, message: '서버 문제인 듯' })
 }
 
 export default errorHandler
